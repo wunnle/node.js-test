18d2fb342289cec8c06612c842a71b70303c8be0
Sinan <me@wunnle.com>
2019-04-25 12:09:27 +0300

    testing a sample server

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-module.exports = (req, res) => {
-  res.end(`Hi!`)
-}
+var http = require('http');
+
+http.createServer(function(request, response) {
+  response.write('Hello universe!')
+  response.end()
+}).listen(8080)
