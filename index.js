bdddf49b527f0df8b1d6295f5ba251858c113652
Sinan <me@wunnle.com>
2019-04-25 12:20:31 +0300

    testing routes

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const app = express()
 
 app.use(helmet())
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.set('Content-Type', 'text/html')
   res.status(200).send(`
     <marquee>Hello world!</marquee>
+  `)
+})
+
+app.get('/secret', (req, res) => {
+  res.set('Content-Type', 'text/html')
+  res.status(200).send(`
+    <h1>This is a secret page!</h1>
   `)
 })
 
