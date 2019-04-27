cba30c507bcd183f9f1eb6184db0e951d5a09b88
wunnle <me@wunnle.com>
2019-04-27 11:59:58 +0300

    learned how to serve static files

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();
 var wiki = require('./wiki')
 
+
+app.use('/public', express.static('public'))
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
 app.use('/wiki', wiki)
+
+app.use(express.static('public'));
+
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
